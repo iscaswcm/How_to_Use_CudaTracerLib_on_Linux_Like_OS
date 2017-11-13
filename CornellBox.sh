a4c989785b9a0207886b5363c4ede18a6ba0d47c
Changmao Wu <changmao@iscas.ac.cn>
2017-11-13 09:18:53 +0800

    Add scripts for model Green_big & CornellBox, in shell mode, just type: $ ./ $ ./

---
+++ CornellBox.sh
@@ -0,0 +1,18 @@
+./hostExample  --model.name cornellbox/cornellbox.obj \
+  --renderer.imageSamples 8 \
+  --renderer.pixelSamples 8 \
+  --image.fileName CornellBox.png \
+  --image.width  1280 \
+  --image.height 1280 \
+  --image.write 1 \
+  --camera.px 0 \
+  --camera.py 1 \
+  --camera.pz 3.5 \
+  --camera.tx 0 \
+  --camera.ty 1 \
+  --camera.tz -3.5 \
+  --camera.ux 0 \
+  --camera.uy 1 \
+  --camera.uz 0 \
+  --camera.fov 43
+
---
+++ Greenbig.sh
@@ -0,0 +1,18 @@
+./hostExample  --model.name Greeble_big/Greeble_big.obj \
+  --renderer.imageSamples 8 \
+  --renderer.pixelSamples 8 \
+  --image.fileName Greeble_big.png \
+  --image.width  1280 \
+  --image.height  720 \
+  --image.write 1 \
+  --camera.px 0 \
+  --camera.py 95 \
+  --camera.pz -170 \
+  --camera.tx 0 \
+  --camera.ty 95 \
+  --camera.tz -169 \
+  --camera.ux 0 \
+  --camera.uy 1 \
+  --camera.uz 0 \
+  --camera.fov 90
+
